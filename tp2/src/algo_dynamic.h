e84ff0a5d4503c2cf17412a91e38de223dc84162
Francis St-Amour <m_psyco2@hotmail.com>
2015-11-11 20:30:14 -0500

    tp2 - Algo dynamique skeleton.

---
+++ tp2/src/algo_dynamic.h
@@ -0,0 +1,13 @@
+#ifndef ALGO_DYNAMIC_H
+#define ALGO_DYNAMIC_H
+
+#include <iostream>
+#include "exemplaire_struct.h"
+#include <algorithm>
+#include <climits>
+
+void mise_en_boite_dynamic(exemplaire* e) {
+
+}
+
+#endif /* end of include guard: ALGO_DYNAMIC_H */
