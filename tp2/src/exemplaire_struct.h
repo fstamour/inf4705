259cff5caa30649ee06fd1342453f68b37954bcc
Francis St-Amour <m_psyco2@hotmail.com>
2015-11-11 19:43:06 -0500

    Added comments on exemplaire struct

--- tp2/src/exemplaire_struct.h
+++ tp2/src/exemplaire_struct.h
@@ -4,17 +4,29 @@
 #include <vector>
 
 struct exemplaire {
+    // Liste des items
     int *data;
+    // Liste des boites
     int *space;
+    // Solution courante
     std::vector<int> *result;
+    // Nombre d'element
     int nb_element;
+    // Nombre de boite
     int nb_box;
+    // Capacite de chaque boite
     int capacity;
+    // Le temps pris pour executer l'algo
     clock_t time;
 };
 
+// Charge un exemplaire.
 struct exemplaire * make_exemplaire(char * filename);
+
+// Libere la memoire d'un exemplaire.
 void free_exemplaire(struct exemplaire * e);
+
+// Affiche la solution trouve pour un exemplaire.
 void print_solution(struct exemplaire * e);
 
 #endif /* EXEMPLAIRE_STRUCT_H_ */
