66e92899482f488513d7252dc81f91ba97cca95d
Marcial <marcial.lopez-ferrada@polymtl.ca>
2015-11-19 17:10:08 -0500

    benchmark recuit simuler avec resultat

---
+++ tp2/src/bench_recuit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+rm -f results_recuit.txt
+RESULT_FILE=results_recuit.txt
+
+for file in `find ../MiseEnBoite -type f | sort -n`; do
+    echo $file
+    ./recuitSimule -pf $file | tee -a $RESULT_FILE
+    echo "---" >> $RESULT_FILE
+done;
