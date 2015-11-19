1774001fc9e8122707c9537156d113187cbdf49c
Francis St-Amour <fr.stamour@gmail.com>
2015-11-19 15:16:11 -0500

    DONE Benchmark pour l'algo dynamique.

---
+++ tp2/src/bench_dynamique.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+rm -f results_dynamique.txt
+RESULT_FILE=results_dynamique.txt
+
+for file in `find ../MiseEnBoite -type f | sort -n`; do
+    echo $file
+    ./dynamique -pf $file | tee -a $RESULT_FILE
+    echo "---" >> $RESULT_FILE
+done;
