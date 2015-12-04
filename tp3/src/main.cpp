835c7a5ba74166cded85cfdf9d972694ca6e6f55
Francis St-Amour <m_psyco2@hotmail.com>
2015-12-04 03:51:17 -0500

    WIP Option parsing, printing solutions (handling the verbose flag).

--- tp3/src/main.cpp
+++ tp3/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unistd.h> // For option parsing.
 
 #include "data.h"
 #include "stats.h"
@@ -26,12 +27,40 @@ void general_stats() {
 
 int main(int argc, char *argv[])
 {
-    // TODO Parse command-line options
-    AlgoRecuit algo(argv[1], 1000);
+    // "Globals"
+    string filename;
+    bool verbose_p = false;
+    // Le nombre d'etape pour le recuit
+    int steps = 100;
+    
+    // Parse command-line options
+    int opt;
+    while((opt = getopt(argc, argv, "pf:s:")) != -1) {
+        switch(opt) {
+            case 'p':
+                verbose_p = true;
+                break;
+            case 'f':
+                filename = optarg;
+                break;
+            case 's':
+                steps = std::stoi(optarg);
+                break;
+            //case '?':
+                //cerr << "Unrecognized option"
+        }
+    }
+    // TODO Check if arguments are valid.
+
+    AlgoRecuit algo(filename, steps);
 
     algo.init_solution();
     while(true){
         algo.run_one_loop();
+        if(algo.new_solution) {
+            algo.print_solution(algo.best_sol, verbose_p);
+            algo.new_solution = false;
+        }
     }
 
 //    general_stats();
