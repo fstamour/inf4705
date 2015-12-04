56bd55f507d0d15736381006f2c4e1e461bf0809
Francis St-Amour <m_psyco2@hotmail.com>
2015-12-04 15:56:38 -0500

    Slightly better opencl test.

--- tp3/src/test_opencl.cpp
+++ tp3/src/test_opencl.cpp
@@ -1,12 +1,25 @@
+#include <iostream>
 #include <vector>
-#include <CL/cl.h>
+
+// OpenCL
+#ifdef __APPLE__
+    #include "OpenCL/opencl.h"
+#else
+    #include "CL/cl.h"
+#endif
+
+using std::cout;
+using std::cerr;
+using std::endl;
 
 int main(int argc, char** arg) {
-	cl_uint platformIdCount = 0;
-	clGetPlatformIDs (0, nullptr, &platformIdCount);
+	cl_uint platform_count = 0;
+	clGetPlatformIDs (0, nullptr, &platform_count);
 
-	std::vector<cl_platform_id> platformIds (platformIdCount);
-	clGetPlatformIDs (platformIdCount, platformIds.data (), nullptr);
+	std::vector<cl_platform_id> platform_id (platform_count);
+	clGetPlatformIDs (platform_count, platform_id.data(), nullptr);
+
+    cout << "There is " << platform_count << " platforms accessible." << endl;
 	
 	return 0;
 }
